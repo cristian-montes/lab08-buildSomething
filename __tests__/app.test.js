fa76635855a7bd7a6a3e29848ba31fb8f1a49a71
Cristian Montes <cmontesmujica@gmail.com>
2021-09-23 15:23:39 -0700

    setting up

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,10 +3,36 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
-describe('demo routes', () => {
+jest.mock('twilio', () => () => ({
+  messages: {
+    create: jest.fn()
+  }
+}));
+
+
+describe('sends tex messages with rick an morty carackters', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  //POST A NEW ORDER TEST
+  it('creates a new order in our database and sends a text message', () => {
+    return request(app)
+      .post('/api/v1/orders')
+      .send({ quantity: 10 })
+      .then(res => {
+        // expect(createMessage).toHaveBeenCalledTimes(1);
+        expect(res.body).toEqual({
+          id: '1',
+          quantity: 10
+        });
+      });
+  });
+
+
+
+
+
 
   afterAll(() => {
     pool.end();
--- data/setup.js
+++ data/setup.js
@@ -1,6 +1,6 @@
 import { promises as fs } from 'fs';
 
 export default (pool) => {
-  return fs.readFile('./sql/setup.sql', { encoding: 'utf-8' })
+  return fs.readFile(`${__dirname}/../sql/setup.sql`, { encoding: 'utf-8' })
     .then(sql => pool.query(sql));
 };
