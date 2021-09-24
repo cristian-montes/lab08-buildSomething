bb87591dbd94ff44368f3575887d51c597ada750
Cristian Montes <cmontesmujica@gmail.com>
2021-09-24 14:04:21 -0700

    post test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -16,14 +16,15 @@ describe('sends tex messages with rick an morty carackters', () => {
     return setup(pool);
   });
 
-  //POST A MESSAGE
+  //POST A QUOTE
   it('creates a new order in our database and sends a text message', () => {
     return request(app)
-      .get('/api/v1/quotesmsg/1')
+      .post('/api/v1/quotesmsg/6')
+      .send({ quote:'Abadango Cluster Princess you are Alive, because your are decedent of Aliens' })
       .then(res => {
         expect(res.body).toEqual({
           id: '1',
-          quote: 'Morty Smith is Human and Alivee'
+          quote: 'Abadango Cluster Princess you are Alive, because your are decedent of Aliens'
         });
       });
 
